deb184c03d8b7a536b75ad67b97164f20696ed0f
Ayush Raj <ayushraj@Piyushs-MacBook-Pro.local>
2024-02-27 18:44:03 +0530

    houseRob

---
+++ Week-4/HouseRobberIIUsingDP.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/house-robber-ii/description/
+public class HouseRobberIIUsingDP { // used top-bottom dp approach
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int n = nums.length;
+        // we need two dp array because the first and last houses can't be robbed at a similar time
+        // as they are adjacent because they are arranged in a circle
+        // Calculate the maximum amount by robbing houses from 0 to n-2
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1, -1);
+        int max1 = robHelper(nums, dp1, 0, n - 1);
+
+        // Calculate the maximum amount by robbing houses from 1 to n-1
+        int[] dp2 = new int[n];
+        Arrays.fill(dp2, -1);
+        int max2 = robHelper(nums, dp2, 1, n);
+
+        return Math.max(max1, max2);
+    }
+
+    private int robHelper(int[] nums, int[] dp, int start, int end) {
+        int n = nums.length;
+        if (start >= end) return 0;
+        if (dp[start] != -1) return dp[start];
+        dp[start] = Math.max(nums[start] + robHelper(nums, dp, start + 2, end), robHelper(nums, dp, start + 1, end));
+        return dp[start];
+    }
+}
--- Week-4/MaxNonAdjacentSumUsingDP.java
+++ Week-4/MaxNonAdjacentSumUsingDP.java
@@ -3,13 +3,14 @@ public class MaxNonAdjacentSumUsingDP { // both top-bottom and bottom-top dp app
     //Function to find the maximum money the thief can get.
     public int FindMaxSum(int arr[], int n)
     {
-        int[] ans = new int[n+1]; // this is bottom-top dp approach (i.e iterative)
-        ans[0] = Math.max(0, arr[0]);
-        ans[1] = Math.max(ans[0], arr[1]);
+        int f = Math.max(0, arr[0]); // this is bottom-top dp approach (i.e iterative)
+        int s = Math.max(arr[1], f);
         for(int i=2;i<n;i++){
-            ans[i] = Math.max(arr[i]+ans[i-2], ans[i-1]);
+            int curr = Math.max(arr[i]+f, s);
+            f = s;
+            s = curr;
         }
-        return ans[n-1];
+        return s;
         //   int[] dp = new int[n+1]; this is top-bottom approach
         //   Arrays.fill(dp, -1);
         //   return maxSum(arr, dp, 0, n);
