0fff31e3b3f6b4b76e2224a5afbe335d0571bfd3
Ayush Raj <ayushraj@Piyushs-MacBook-Pro.local>
2024-03-16 20:59:44 +0530

    change

--- Week-4/LongestConsecutiveSequence.java
+++ Week-4/LongestConsecutiveSequence.java
@@ -2,27 +2,26 @@
 import java.util.*;
 public class LongestConsecutiveSequence { // used streak logic
     public int longestConsecutive(int[] nums) {
-        if (nums == null || nums.length == 0) // Handle edge case: empty array
-            return 0;
+        Set<Integer> map = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
 
-        Map<Integer, Integer> map = new HashMap<>(); // Map to store the streaks
-        int ans = 0;
-
-        for(int num : nums) {
-            if (!map.containsKey(num)) {
-                int leftStreak = map.getOrDefault(num - 1, 0); // Get streak of left adjacent number
-                int rightStreak = map.getOrDefault(num + 1, 0); // Get streak of right adjacent number
-                int currentStreak = leftStreak + rightStreak + 1; // Calculate current streak
-
-                ans = Math.max(ans, currentStreak); // Update the maximum streak
-
-                // Update streaks for current number and its neighbors
-                map.put(num, currentStreak);
-                map.put(num - leftStreak, currentStreak);
-                map.put(num + rightStreak, currentStreak);
-            }
+        for(int i : nums){
+            map.add(i);
         }
 
+        int ans = 0;
+        for(int i=0;i<nums.length;i++){
+            if(map.contains(nums[i]-1)) continue;
+            if(visited.contains(nums[i])) continue;
+            visited.add(nums[i]);
+            int len = 0;
+            int x = nums[i];
+            while(map.contains(x)){
+                len++;
+                x++;
+            }
+            ans = Math.max(ans, len);
+        }
         return ans;
     }
 }
